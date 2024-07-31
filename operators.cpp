44b93c51604971b6dc7db2e454b82e5cf9695d72
DeeptiEmmi <emmideepti21@gmail.com>
2024-07-31 21:01:26 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout<<"enter number to check ";
+    cin>>x;
+    if (x>0)
+      cout<<"integer is positive";
+   
+    else
+       cout<<"integer is negative";
+
+}
+
+///enter number to check -21
+//integer is negative
+
+//=== Code Execution Successful ===
+//enter number to check 22
+//integer is positive
+
+//=== Code Execution Successful ===
